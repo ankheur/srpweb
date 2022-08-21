50d7333fe7d4d02e2f83ba969bb19fc2fda49f69
Pierrick Rancoeur <pierre.ankh@gmail.com>
2022-08-21 16:29:39 +0200

    Fix sitename link to homepage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <div class="mx-auto max-w-7xl">
             <nav class="flex items-center w-full h-24 select-none" x-data="{ showMenu: false }">
                 <div class="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
-                    <a href="#_" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
+                    <a href="/" class="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
                         <span class="p-1 text-xl font-black leading-none text-indigo-600 select-none"><span>SRPWeb</span></span>
                     </a>
                     <div class="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-indigo-600 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex" :class="{'flex': showMenu, 'hidden': !showMenu }">
