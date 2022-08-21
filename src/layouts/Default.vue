e3b72b1de57760f13853566e364719d1937bf26a
Pierrick Rancoeur <pierre.ankh@gmail.com>
2022-08-21 16:46:29 +0200

    Clean and refix tailwind

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="font-sans">
+  <div>
     <slot></slot>
     <footer class="bg-white">
       <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
